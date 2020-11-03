32267890b372696dbfdca56ce49676db382d8707
Wayne <kimjihyo0325@gmail.com>
2020-11-04 05:06:07 +0900

    added on click listners

--- android/app/src/main/java/com/example/discoverar/AccountCreationActivity.java
+++ android/app/src/main/java/com/example/discoverar/AccountCreationActivity.java
@@ -3,12 +3,24 @@ package com.example.discoverar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountCreationActivity extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
+
+        backButton = (Button) findViewById(R.id.sign_up_screen_go_bacl_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- android/app/src/main/java/com/example/discoverar/LoginActivity.java
+++ android/app/src/main/java/com/example/discoverar/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.discoverar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView signUpButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        signUpButton = (TextView) findViewById(R.id.login_screen_sign_up_button);
+
+        signUpButton.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, AccountCreationActivity.class));
+            }
+        });
     }
 }
